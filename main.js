a804f400ee1a4cd166ce9676b55fe37584c6936b
Jufebrown <jufebrown@gmail.com>
2017-03-07 09:46:41 -0600

    added record to employee table

--- main.js
+++ main.js
@@ -5,4 +5,6 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('example.sqlite')
 
-db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT)')
+db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary NUMBER(6, 2))')
+
+db.run(`INSERT INTO employees VALUES (1, "Ashley", "Irwin", 5)`)
