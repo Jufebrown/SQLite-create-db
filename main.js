63bcd9688570c1fd6a1889330d132e9794c55a28
Jufebrown <jufebrown@gmail.com>
2017-03-07 10:24:07 -0600

    added department field to employees table

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const dropEmployees = () => {
 
 // dropEmployees()
 
-db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary DECIMAL(6, 2))')
+db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary DECIMAL(6, 2), department TEXT)')
 
 // db.run(`INSERT INTO employees VALUES (1, "Jufe", "Brown-Tsai", 125000.86)`)
 
@@ -23,7 +23,8 @@ const populateEmployees = () => {
       ${each.id},
       "${each.firstName}",
       "${each.lastName}",
-      ${each.salary}
+      ${each.salary},
+      "${each.department}"
     )`)
   })
 }
