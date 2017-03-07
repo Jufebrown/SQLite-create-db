ea70c1b96ca53c617a9aeab9e60c6cd7ae756f9d
Jufebrown <jufebrown@gmail.com>
2017-03-07 09:41:39 -0600

    added table

--- main.js
+++ main.js
@@ -5,4 +5,4 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('example.sqlite')
 
-console.log(db)
+db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT)')
